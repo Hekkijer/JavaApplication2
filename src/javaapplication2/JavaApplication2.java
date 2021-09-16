3d442be47eaedb30e9a6a047fd1a19be7fdf71e7
Your Name <you@example.com>
2021-09-16 18:13:56 +0300

    1

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* TASK 1.2 number -> nums/decs/sum */
+       Scanner in = new Scanner(System.in);
+       System.out.print("Input 3-mark number: ");
+       int number = in.nextInt();
+        
+       int numbers = number%10;
+       System.out.printf("Numbers: %d \n", numbers);
+       
+       int decs = number%100/10;
+       System.out.printf("Decs: %d \n", decs);
+       
+       int sum = number/100 + numbers + decs;
+       System.out.printf("Sum of all 3 numbers: %d \n", sum);
+    }
+    
+}
